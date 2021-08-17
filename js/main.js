6c07a1d2a8f29a034eef0456ac2c05e53fe99223
Mark Hansen <mhansen@accusoft.com>
2021-08-17 18:15:49 -0400

    Touch support, better rendering on mobile

--- js/main.js
+++ js/main.js
@@ -4,35 +4,93 @@ let currentEpisode = 0;
 function init() {
     loadBoard().then(result => {
         snls = result;
-        setEpisode();
+        setCard();
     });
-
     setListeners();
+}
 
+function setCard() {
+    
+    
+    var height = window.innerHeight;
+    var width = window.innerWidth;
+
+    // TODO: we should compute the with the border set in css
+    // padding = 35 = (70 * 3) = 240px in padding
+    // margin-top on cards = 10px = 20
+    // body margin top = 40
+
+    height = (height - 320) / 3;
+
+    var card = document.getElementById("card-date");
+    card.style.height = height;
+    card.style.width = (height) * (5 / 3);
+
+    var card = document.getElementById("card-host");
+    card.style.height = height;
+    card.style.width = height * (5 / 3);
+
+    var card = document.getElementById("card-music");
+    card.style.height = height;
+    card.style.width = height * (5 / 3);
+
+    setEpisode();
 }
 
 function setListeners() {
 
+    let mc = new Hammer.Manager(document.getElementById("body"));
+    //mc.get('swipe').set({ direction: Hammer.DIRECTION_ALL });
+    mc.add ( new Hammer.Swipe({ direction: Hammer.DIRECTION_HORIZONTAL }));
+    mc.add( new Hammer.Tap({ event: 'doubletap', taps: 2 }) );
+
+
+    mc.on("swipeleft swiperight swipedown doubletap", function(ev) {
+  
+         if (ev.type === 'swiperight')
+            nextEpisode();
+        else if (ev.type === 'swipeleft')
+            prevEpisode();
+        else if (ev.type === 'doubletap')
+            pickRandom();
+
+    });
+
+    window.addEventListener('orientationchange', function(event) {
+        //setCard();
+    })
+
+    window.addEventListener('resize', function() {
+        setCard();
+    });
+
     document.addEventListener('keydown', function (e) {
 
         if (e.key === 'ArrowRight') {
-            if (currentEpisode - 1 >= 0) {
-                currentEpisode--;
-                setEpisode();
-            }
+            nextEpisode();
         }
 
         if (e.key === 'ArrowLeft') {
-            if (currentEpisode + 1 < snls.seasons.length) {
-                currentEpisode++;
-                setEpisode();
-            }
+            prevEpisode();
         }
 
         if (e.key.toLowerCase() === 'r') {
             pickRandom();
         }
     });
+}
+
+function nextEpisode() {
+    if (currentEpisode - 1 >= 0) {
+        currentEpisode--;
+        setEpisode();
+    }
+}
+function prevEpisode() {
+    if (currentEpisode + 1 < snls.seasons.length) {
+        currentEpisode++;
+        setEpisode();
+    }
 }
 
 function setEpisode() {
@@ -64,7 +122,7 @@ function fitFont(elm) {
     var paddingHorz = parseFloat(outer.paddingRight) + parseFloat(outer.paddingLeft);
     var paddingVert = parseFloat(outer.paddingTop) + parseFloat(outer.paddingBottom);
 
-    for (let size = 120; size > 10; size--) {
+    for (let size = 140; size > 10; size--) {
         elm.style.fontSize = size + "px";
         elm.style.lineHeight = size + "px";
 
@@ -76,7 +134,7 @@ function fitFont(elm) {
 
 
 async function loadBoard() {
-    const response = await fetch('../data/seasons.json');
+    const response = await fetch('./data/seasons.json');
     const shows = await response.json();
     return shows;
 }
